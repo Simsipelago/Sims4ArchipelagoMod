3b83e20fc224d611fcbbdf4279a9fb5e7a1057fa
Benny Dreamly <bgaming0277@gmail.com>
2025-01-06 22:02:24 -0700

    edit compile script

--- Scripts/compile/compile.py
+++ Scripts/compile/compile.py
@@ -7,6 +7,6 @@ release_dir = os.path.join('..', '..', 'Release', 'S4CLSampleMod')
 # This function invocation will compile the files found within Scripts/s4cl_sample_mod_scripts, put them inside of a file named s4cl_sample_mod.ts4script, and it will finally place that ts4script file within <Project>/Release/S4CLSampleMod.
 Unpyc3PythonCompiler.compile_mod(
     folder_path_to_output_ts4script_to=release_dir,
-    names_of_modules_include=('s4cl_sample_mod_scripts',),
+    names_of_modules_include=('s4ap', 'websocket', 'rel'),
     output_ts4script_name='s4cl_sample_mod'
 )
