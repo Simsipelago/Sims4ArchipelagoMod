a62024e1d4b1458a1c12dff128bd84c033a73ad8
Benny Dreamly <bgaming0277@gmail.com>
2025-09-05 14:31:02 -0600

    comment out the old command approach (not deleting it *yet*)

--- Scripts/s4ap/s4ap_commands.py
+++ Scripts/s4ap/s4ap_commands.py
@@ -1,8 +1,8 @@
 from s4ap.logging.s4ap_logger import S4APLogger
 from s4ap.modinfo import ModInfo
 from sims4.commands import Command, CommandType, Output
-from sims4communitylib.services.commands.common_console_command import CommonConsoleCommand
-from sims4communitylib.services.commands.common_console_command_output import CommonConsoleCommandOutput
+# from sims4communitylib.services.commands.common_console_command import CommonConsoleCommand
+# from sims4communitylib.services.commands.common_console_command_output import CommonConsoleCommandOutput
 
 log = S4APLogger.get_log()
 log.enable()
@@ -13,6 +13,6 @@ def show_mod_version(_connection=None):
     output = Output(_connection)
     output(f"The Sims 4 Archipelago, Current Mod Version: {ModInfo.get_identity().version}")
 
-@CommonConsoleCommand(ModInfo.get_identity(), 's4ap.version', 'Prints the mod version to the console')
-def _show_mod_version(output: CommonConsoleCommandOutput):
-    output(f"The Sims 4 Archipelago, Current Mod Version: {ModInfo.get_identity().version}")
+# @CommonConsoleCommand(ModInfo.get_identity(), 's4ap.version', 'Prints the mod version to the console')
+# def _show_mod_version(output: CommonConsoleCommandOutput):
+#     output(f"The Sims 4 Archipelago, Current Mod Version: {ModInfo.get_identity().version}")
