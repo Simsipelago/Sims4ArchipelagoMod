e55b7935242750c57d9bccb0f0a3cfb85ad8f31d
Benny Dreamly <bgaming0277@gmail.com>
2025-09-05 14:32:47 -0600

    change version

--- Scripts/s4ap/modinfo.py
+++ Scripts/s4ap/modinfo.py
@@ -14,7 +14,7 @@ class ModInfo(CommonModInfo):
     @property
     def _version(self) -> str:
         # Mod version
-        return '0.3.0-rc1'
+        return '0.3.0-rc2'
 
     @property
     def _author(self) -> str:
