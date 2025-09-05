7a1f5330701c859319cb258449f57ff43cdf3f16
Benny Dreamly <bgaming0277@gmail.com>
2025-09-05 10:14:10 -0600

    purge another s4cl import in the career event file

--- Scripts/s4ap/events/career_event_dispatcher.py
+++ Scripts/s4ap/events/career_event_dispatcher.py
@@ -3,12 +3,12 @@ from s4ap.enums.S4APLocalization import HashLookup
 from s4ap.events.checks.send_check_event import SendLocationEvent
 from s4ap.logging.s4ap_logger import S4APLogger
 from s4ap.modinfo import ModInfo
+from s4ap.utils.s4ap_household_utils import S4APHouseholdUtils
 from s4ap.utils.s4ap_sim_utils import S4APSimUtils
 from sims4communitylib.events.event_handling.common_event import CommonEvent
 from sims4communitylib.events.event_handling.common_event_registry import CommonEventRegistry
 from sims4communitylib.services.common_service import CommonService
 from sims4communitylib.utils.common_injection_utils import CommonInjectionUtils
-from sims4communitylib.utils.sims.common_household_utils import CommonHouseholdUtils
 
 log = S4APLogger.get_log()
 log.enable()
@@ -44,7 +44,7 @@ def _on_milestone_complete(original, self, *args, **kwargs):
     career = self._current_track.get_career_name(self._sim_info).hash
     level = self._user_level
     sim_info = self._sim_info
-    if sim_info in CommonHouseholdUtils.get_sim_info_of_all_sims_in_active_household_generator():
+    if sim_info in S4APHouseholdUtils.get_sim_info_of_all_sims_in_active_household_generator():
         sim_name = S4APSimUtils.get_sim_first_name(sim_info)
         OnCareerPromotionEvent.get()._on_promotion(career, level, sim_name)
     return result
