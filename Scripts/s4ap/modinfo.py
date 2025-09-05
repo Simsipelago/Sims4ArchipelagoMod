b0c788903d4179fbf38c399f37d011174c067117
Benny Dreamly <bgaming0277@gmail.com>
2025-09-04 20:11:18 -0600

    new version

--- Scripts/s4ap/modinfo.py
+++ Scripts/s4ap/modinfo.py
@@ -14,7 +14,7 @@ class ModInfo(CommonModInfo):
     @property
     def _version(self) -> str:
         # Mod version
-        return '0.2.1'
+        return '0.3.0-rc1'
 
     @property
     def _author(self) -> str:
