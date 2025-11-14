e7ac6ac8a040a9e3cb14464bb5d40878c3fe51db
Benny Dreamly <bgaming0277@gmail.com>
2025-11-13 18:00:38 -0700

    increment version with new letter

--- Scripts/s4ap/modinfo.py
+++ Scripts/s4ap/modinfo.py
@@ -14,7 +14,7 @@ class ModInfo(CommonModInfo):
     @property
     def _version(self) -> str:
         # Mod version
-        return '0.2.5a'
+        return '0.2.5b'
 
     @property
     def _author(self) -> str:
