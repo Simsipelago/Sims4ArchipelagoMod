895d640e55bfb63ca7f5e2c42af9b66d6ae5153b
Benny Dreamly <bgaming0277@gmail.com>
2025-12-04 15:31:39 -0700

    oops fix the missing import

--- Scripts/s4ap/utils/s4ap_career_utils.py
+++ Scripts/s4ap/utils/s4ap_career_utils.py
@@ -1,6 +1,6 @@
 from careers.career_tuning import Career, CareerLevel, TunableCareerTrack
 from random import random
-from typing import Callable, Iterator, Tuple, Union
+from typing import Callable, Iterator, Tuple, Union, List
 from services import get_instance_manager
 from sims.sim_info import SimInfo
 from sims4.resources import Types
