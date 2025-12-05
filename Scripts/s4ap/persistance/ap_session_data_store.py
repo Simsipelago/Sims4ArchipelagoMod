bde15d339efdc9ecaee2b259e3f03f4a04aa5efe
Benny Dreamly <bgaming0277@gmail.com>
2025-12-04 17:41:50 -0700

    update the docstring

--- Scripts/s4ap/persistance/ap_session_data_store.py
+++ Scripts/s4ap/persistance/ap_session_data_store.py
@@ -24,7 +24,7 @@ class S4APSessionStoreUtils:
 
     def check_session_values(self, host_name: str, port: int, seed_name: str, player: str, slot: int) -> bool:
         """ Check session store to make sure it's the same settings as before and send a warning otherwise
-            :returns False, if settings don't exist or equal those that were used before """
+            :returns True, if stored settings exist and all values match the incoming parameters; False otherwise. """
 
         stored_seed = self._get_value(S4APSettings.SEED_NAME)
 
