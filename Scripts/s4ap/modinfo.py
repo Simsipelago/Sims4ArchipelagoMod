b683ba67ce1db69d7b96e603671fd31551828f8d
Benny Dreamly <bgaming0277@gmail.com>
2025-12-16 10:14:39 -0700

    update the mod version

--- Scripts/s4ap/modinfo.py
+++ Scripts/s4ap/modinfo.py
@@ -14,7 +14,7 @@ class ModInfo(CommonModInfo):
     @property
     def _version(self) -> str:
         # Mod version
-        return '0.3.0.beta2'
+        return '0.3.0.beta3'
 
     @property
     def _author(self) -> str:
